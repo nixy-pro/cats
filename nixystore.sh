7dbc1a2c84217a39f2bd59cb93f1350a22cf90dd
NIXY Project <106488921+nixy-pro@users.noreply.github.com>
2024-10-10 01:25:33 +0800

    NIXY Project

--- nixystore.sh
+++ nixystore.sh
@@ -27,7 +27,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/anggabadol/Autoscript-tunneling-premium/main/menu/menu.zip
+    wget https://vip.nixystore.my.id/anggabadol/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
