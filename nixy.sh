29f780975e4b939d89dbf7eea740821421ed765e
NIXY Project <106488921+nixy-pro@users.noreply.github.com>
2024-10-10 06:00:00 +0800

    NIXY Project

--- nixy.sh
+++ nixy.sh
@@ -277,7 +277,7 @@ restart_system(){
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
-izinsc="https://vip.nixystore.my.id/register"
+izinsc="https://izin.nixystore.my.id/ip"
 # USERNAME
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}')
